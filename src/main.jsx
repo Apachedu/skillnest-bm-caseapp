69166c7f8cac61c46245592e633534d18092e7e3
Anamika Pachauri <anamikapachauri@Anamikas-MacBook-Air.local>
2025-07-31 00:08:13 +0530

    Fixed routing to enable individual case view

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import './index.css'; // make sure this path is valid
+import CaseStudyPage from './components/CaseStudyPage';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/case/:id" element={<CaseStudyPage />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
