a7cddd548845da04133d3e820f776813378d6e00
Anamika Pachauri <anamikapachauri@Anamikas-MacBook-Air.local>
2025-08-02 13:58:25 +0530

    Initial commit

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Login } from './Login';
-export { default as Register } from './Register';
-export { default as PrivateRoute } from './PrivateRoute';
-export { default as CaseStudyPage } from './CaseStudyPage';
-export { default as CaseStudyList } from './CaseStudyList';
-export { default as DashboardPage } from './DashboardPage';
